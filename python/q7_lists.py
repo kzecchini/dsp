39608a03cb9484d3a24b7f6cbcd030532721bea6
Kevin Zecchini <kzecchini@Kevins-MacBook-Pro.local>
2015-12-17 19:15:56 -0500

    Completed editing all functions.

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -15,7 +15,11 @@ def match_ends(words):
     >>> match_ends(['aaa', 'be', 'abc', 'hello'])
     1
     """
-    raise NotImplementedError
+    count = 0
+    for word in words:
+        if len(word) >= 2 and (word[0] == word[-1]):
+            count += 1
+    return count
 
 
 def front_x(words):
@@ -32,7 +36,14 @@ def front_x(words):
     >>> front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
     ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
     """
-    raise NotImplementedError
+    xlist = []
+    otherlist = []
+    for word in words:
+        if word[0] == 'x':
+            xlist.append(word)
+        else:
+            otherlist.append(word)
+    return sorted(xlist) + sorted(otherlist)
 
 
 def sort_last(tuples):
@@ -49,7 +60,7 @@ def sort_last(tuples):
     >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
     [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
     """
-    raise NotImplementedError
+    return sorted(tuples, key=lambda x: x[-1])
 
 
 def remove_adjacent(nums):
@@ -68,7 +79,11 @@ def remove_adjacent(nums):
     >>> remove_adjacent([])
     []
     """
-    raise NotImplementedError
+    result = []
+    for num in nums:
+        if len(result) == 0 or num != result[-1]:
+            result.append(num)
+    return result
 
 
 def linear_merge(list1, list2):
@@ -85,4 +100,12 @@ def linear_merge(list1, list2):
     >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
     ['aa', 'aa', 'aa', 'bb', 'bb']
     """
-    raise NotImplementedError
+    result = []
+    while len(list1) and len(list2):
+        if list1[0] > list2[0]:
+            result.append(list2.pop(0))
+        else:
+            result.append(list1.pop(0))
+    result.extend(list1)
+    result.extend(list2)
+    return result
