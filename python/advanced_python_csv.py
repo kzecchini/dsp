14ef841b8583f45afc2c59b9a4d7572f93848dd0
Kevin Zecchini <kzecchini@kevins-mbp.vinceraservices.net>
2015-12-18 12:53:57 -0500

    Finished csv code

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,20 @@
-PLACE YOUR CODE HERE
+import csv
+from advanced_python_regex import Faculty
+
+
+def write_emails(data):
+    '''
+    Given an initial csv file, will create a list of emails and write those
+    to a new csv file with the title 'emails.csv'.
+    '''
+    original_csv = Faculty(data)
+    emails = original_csv.emails()
+    f = open('emails.csv', 'w')
+    fieldnames = ['email']
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+    writer.writeheader()
+    for email in emails:
+        writer.writerow({'email': email})
+    f.close()
+
+write_emails('faculty.csv')
